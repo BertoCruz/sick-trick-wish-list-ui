c829295692bbff5a21db9095edbaa48e32fc94ee
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2023-01-02 15:39:37 -0600

    display existing tricks to DOM

--- src/components/TrickCard/TrickCard.js
+++ src/components/TrickCard/TrickCard.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function TrickCard({id, stance, name, obstacle, tutorial}) {
-
+function TrickCard({ stance, name, obstacle, tutorial }) {
   return (
-    <div className='trick-card'>
-        TrickCard
+    <div className="trick-card">
+      <p>
+        {stance} {name}
+      </p>
+      <p>Obstacle: {obstacle}</p>
+      <p>Link to Tutorial: </p>
+      <a href={tutorial}>{tutorial}</a>
     </div>
-  )
+  );
 }
 
-export default TrickCard
+export default TrickCard;
--- src/components/TricksList/TrickList.js
+++ src/components/TricksList/TrickList.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import TrickCard from '../TrickCard/TrickCard'
+import React from "react";
+import TrickCard from "../TrickCard/TrickCard";
 
-function TrickList({tricks}) {
-    const allTricks = tricks.map(trick => {
-        return (
-            <TrickCard 
-                key={trick.id}
-                id={trick.id}
-                stance={trick.stance}
-                name={trick.name}
-                obstacle={trick.obstacle}
-                tutorial={trick.tutorial}
-            />
-        )
-    })
+function TrickList({ tricks }) {
+  const allTricks = tricks.map((trick) => {
+    return (
+      <TrickCard
+        key={trick.id}
+        id={trick.id}
+        stance={trick.stance}
+        name={trick.name}
+        obstacle={trick.obstacle}
+        tutorial={trick.tutorial}
+      />
+    );
+  });
 
-  return (
-    <div className='trick-list-container'>
-        { allTricks }
-    </div>
-  )
+  return <div className="trick-list-container">{allTricks}</div>;
 }
 
-export default TrickList
+export default TrickList;
